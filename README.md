9f60275d6c172997d4e36e47db4a12182eb9e2ac
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2020-07-10 08:24:55 +0700

    1st: the first upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to nguyentrinhan.dev repository
+this repo contains the source code of a simple Java applicaton used to demonstrate the CI - Continous Itegration process - a part of Introduction to Software Engneering course at FPT University - HCMC Campus
+
+## You will find in this repo:
+* MathUtil class - holds the list of math methods
+* Test class - demostrate on Unit Testing using TDD
+* .gitignore
+* Markdown file - this file itself
+
+## Contact me via
+1. [Github](https://github.com/nguyentrinhan-dev)
+2. [Facebook](https://facebook.com/nguyentrinhan.811)
+
+#### © 2020 by nguyentrinhan.dev
---
+++ src/taptap/util/MathUtil.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taptap.util;
+
+/**
+ *
+ * @author Lenovo day la class chua cac ham toan hoc tinh toan voi muc tieu xai
+ * chung cho cac noi khac, pham cai gi mang tinh chat xai chung ta xe dung
+ * STATIC
+ */
+public class MathUtil {
+
+    /**
+     * vi giai thua tang so luong rat nhanh nen se tran kieu int nen ta xai long
+     * cho an toan
+     *
+     * @param n
+     * @return n! thuc ra 15! da to vl roi nen ta chi tinh giai thua tu 15 tro
+     * lai
+     */
+    public static long computeFactorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
